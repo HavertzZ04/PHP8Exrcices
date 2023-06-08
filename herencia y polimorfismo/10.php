0a6dad06ddc267d2eab73430d1b38c6f1e2986b0
havertzz04 <havertzz04@gmail.com>
2023-06-08 09:18:40 -0500

    feat: :memo: Classes Exercices

--- herencia y polimorfismo/10.php
+++ herencia y polimorfismo/10.php
@@ -16,10 +16,70 @@
         public $origen;
         public $destino;
         public $fecha;
+        public $hora;
+        public $asientos_disponibles;
+
+        public function __construct($origen, $destino, $fecha, $hora, $asientos_disponibles){
+            $this-> origen = $origen;
+            $this-> destino = $destino;
+            $this-> fecha = $fecha;
+            $this-> hora = $hora;
+            $this-> asientos_disponibles = $asientos_disponibles;
+        }
     }
 
+    class Pasajero{
+        public $nombre;
+        public $apellido;
+        public $edad;
+        private $documento;
 
+        public function __construct($nombre, $apellido, $edad, $documento){
+            $this-> nombre = $nombre;
+            $this-> apellido = $apellido;
+            $this-> edad = $edad;
+            $this-> documento = $documento;
+        }
+    }
 
+    class Reserva{
+        public $vuelo;
+        public $pasajero;
+
+        public function __construct($vuelo, $pasajero){
+            $this-> vuelo = $vuelo;
+            $this-> pasajero = $pasajero;
+        }
+
+        public function realizarReserva(){
+
+        }
+    }
+
+    class GestorVuelos{
+        public $vuelos = [];
+
+        public function __construct($vuelos){
+            $this-> vuelos = $vuelos = [];
+        }
+
+        public function mostrarVuelosDisponible(){
+
+        }
+
+    }
+
+    class GestorReservas{
+        public $reservas = [];
+
+        public function __construct($reservas){
+            $this-> reservas = $reservas = [];
+        }
+
+        public function mostrarReservas(){
+
+        }
+    }
 
 
 ?>
